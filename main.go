b2ff585860e3158a1d3df557514744032aed24b1
BenRub <benrub@gmail.com>
2021-03-14 18:22:13 +0200

    Creating "CBContainersGuardrails" api

--- main.go
+++ main.go
@@ -86,6 +86,14 @@ func main() {
 		setupLog.Error(err, "unable to create controller", "controller", "CBContainersCluster")
 		os.Exit(1)
 	}
+	if err = (&controllers.CBContainersGuardrailsReconciler{
+		Client: mgr.GetClient(),
+		Log:    ctrl.Log.WithName("controllers").WithName("CBContainersGuardrails"),
+		Scheme: mgr.GetScheme(),
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "CBContainersGuardrails")
+		os.Exit(1)
+	}
 	// +kubebuilder:scaffold:builder
 
 	if err := mgr.AddHealthzCheck("health", healthz.Ping); err != nil {
