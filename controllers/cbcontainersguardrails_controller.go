65b7e80195c53e19cc6313887a4960b22ce9784e
BenRub <benrub@gmail.com>
2021-03-18 15:54:34 +0200

    Update the role set for controllers with new line

--- controllers/cbcontainersguardrails_controller.go
+++ controllers/cbcontainersguardrails_controller.go
@@ -37,6 +37,7 @@ type CBContainersGuardrailsReconciler struct {
 // +kubebuilder:rbac:groups=operator.containers.carbonblack.io,resources=cbcontainersguardrails,verbs=get;list;watch;create;update;patch;delete
 // +kubebuilder:rbac:groups=operator.containers.carbonblack.io,resources=cbcontainersguardrails/status,verbs=get;update;patch
 // +kubebuilder:rbac:groups=operator.containers.carbonblack.io,resources=cbcontainersguardrails/finalizers,verbs=update
+
 func (r *CBContainersGuardrailsReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
 	_ = r.Log.WithValues("cbcontainersguardrails", req.NamespacedName)
 
