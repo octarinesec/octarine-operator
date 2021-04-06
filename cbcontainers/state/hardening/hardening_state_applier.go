66af1d4fb0f1ec31e2bdb657b2cf48f202e50f0c
BenRub <benrub@gmail.com>
2021-04-06 15:57:32 +0300

    Rename state applier

--- cbcontainers/state/hardening/hardening_state_applier.go
+++ cbcontainers/state/hardening/hardening_state_applier.go
@@ -11,15 +11,15 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/client"
 )
 
-type CBContainersClusterStateApplier struct {
+type HardeningStateApplier struct {
 	enforcerTlsSecret  *hardeningObjects.EnforcerTlsK8sObject
 	enforcerDeployment *hardeningObjects.EnforcerK8sObject
 	enforcerService    *hardeningObjects.EnforcerServiceK8sObject
 	enforcerWebhook    *hardeningObjects.EnforcerWebhookK8sObject
 }
 
-func NewHardeningStateApplier(tlsSecretsValuesCreator hardeningObjects.TlsSecretsValuesCreator) *CBContainersClusterStateApplier {
-	return &CBContainersClusterStateApplier{
+func NewHardeningStateApplier(tlsSecretsValuesCreator hardeningObjects.TlsSecretsValuesCreator) *HardeningStateApplier {
+	return &HardeningStateApplier{
 		enforcerTlsSecret:  hardeningObjects.NewEnforcerTlsK8sObject(tlsSecretsValuesCreator),
 		enforcerDeployment: hardeningObjects.NewEnforcerDeploymentK8sObject(),
 		enforcerService:    hardeningObjects.NewEnforcerServiceK8sObject(),
@@ -27,7 +27,7 @@ func NewHardeningStateApplier(tlsSecretsValuesCreator hardeningObjects.TlsSecret
 	}
 }
 
-func (c *CBContainersClusterStateApplier) ApplyDesiredState(ctx context.Context, cbContainersHardening *cbcontainersv1.CBContainersHardening, client client.Client, setOwner applymentOptions.OwnerSetter) (bool, error) {
+func (c *HardeningStateApplier) ApplyDesiredState(ctx context.Context, cbContainersHardening *cbcontainersv1.CBContainersHardening, client client.Client, setOwner applymentOptions.OwnerSetter) (bool, error) {
 	applyOptions := applymentOptions.NewApplyOptions().SetOwnerSetter(setOwner)
 
 	mutatedSecret, secretK8sObject, err := ApplyHardeningChildK8sObject(ctx, cbContainersHardening, client, c.enforcerTlsSecret, applyOptions, applymentOptions.NewApplyOptions().SetCreateOnly(true))
