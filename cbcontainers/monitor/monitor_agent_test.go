8be68b0050dfda147288bb0cf82020f1c97fbc13
BenRub <benrub@gmail.com>
2021-05-02 14:39:15 +0300

    Extract test enabled components login to a func

--- cbcontainers/monitor/monitor_agent_test.go
+++ cbcontainers/monitor/monitor_agent_test.go
@@ -49,23 +49,26 @@ func testMonitorAgent(t *testing.T, setup SetupFunc) {
 }
 
 func TestEnabledComponents(t *testing.T) {
+	testEnabledComponents := func(t *testing.T, hardeningEnabled, runtimeEnabled bool) {
+		testMonitorAgent(t, func(agent *MonitorAgent, testObjects *TestMonitorObjects) models.HealthReportMessage {
+			testObjects.featuresMock.EXPECT().HardeningEnabled().Return(hardeningEnabled, nil)
+			testObjects.featuresMock.EXPECT().RuntimeEnabled().Return(runtimeEnabled, nil)
+			testObjects.healthCheckerMock.EXPECT().GetPods().Return(map[string]coreV1.Pod{}, nil)
+			testObjects.healthCheckerMock.EXPECT().GetDaemonSets().Return(map[string]appsV1.DaemonSet{}, nil)
+			testObjects.healthCheckerMock.EXPECT().GetDeployments().Return(map[string]appsV1.Deployment{}, nil)
+			testObjects.healthCheckerMock.EXPECT().GetReplicaSets().Return(map[string]appsV1.ReplicaSet{}, nil)
+			testObjects.healthCheckerMock.EXPECT().GetValidatingWebhookConfigurations().Return(map[string]admissionsV1beta1.ValidatingWebhookConfiguration{}, nil)
+
+			return models.NewHealthReportMessage(Account, Cluster, Version, map[string]bool{HardeningFeature: hardeningEnabled, RuntimeFeature: runtimeEnabled}, map[string]models.WorkloadHealthReport{}, map[string]models.WebhookHealthReport{})
+		})
+	}
+
 	possibleFlags := []bool{true, false}
 	for _, hardeningEnabled := range possibleFlags {
 		for _, runtimeEnabled := range possibleFlags {
 			t.Run(fmt.Sprintf("When Hardening is set to %v and Runtime is set to %v, message should be built properly", hardeningEnabled, runtimeEnabled), func(t *testing.T) {
-				testMonitorAgent(t, func(agent *MonitorAgent, testObjects *TestMonitorObjects) models.HealthReportMessage {
-					testObjects.featuresMock.EXPECT().HardeningEnabled().Return(hardeningEnabled, nil)
-					testObjects.featuresMock.EXPECT().RuntimeEnabled().Return(runtimeEnabled, nil)
-					testObjects.healthCheckerMock.EXPECT().GetPods().Return(map[string]coreV1.Pod{}, nil)
-					testObjects.healthCheckerMock.EXPECT().GetDaemonSets().Return(map[string]appsV1.DaemonSet{}, nil)
-					testObjects.healthCheckerMock.EXPECT().GetDeployments().Return(map[string]appsV1.Deployment{}, nil)
-					testObjects.healthCheckerMock.EXPECT().GetReplicaSets().Return(map[string]appsV1.ReplicaSet{}, nil)
-					testObjects.healthCheckerMock.EXPECT().GetValidatingWebhookConfigurations().Return(map[string]admissionsV1beta1.ValidatingWebhookConfiguration{}, nil)
-
-					return models.NewHealthReportMessage(Account, Cluster, Version, map[string]bool{HardeningFeature: hardeningEnabled, RuntimeFeature: runtimeEnabled}, map[string]models.WorkloadHealthReport{}, map[string]models.WebhookHealthReport{})
-				})
+				testEnabledComponents(t, hardeningEnabled, runtimeEnabled)
 			})
 		}
 	}
-
 }
