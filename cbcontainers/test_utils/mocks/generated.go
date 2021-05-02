36e7a87d36b51158999c4c308aa462fdb7afbc49
BenRub <benrub@gmail.com>
2021-05-02 19:43:27 +0300

    add mock for k8s client interface

--- cbcontainers/test_utils/mocks/generated.go
+++ cbcontainers/test_utils/mocks/generated.go
@@ -1,3 +1,4 @@
 package mocks
 
 //go:generate mockgen -destination mock_logger.go -package mocks github.com/go-logr/logr Logger
+//go:generate mockgen -destination mock_k8s_client.go -package mocks sigs.k8s.io/controller-runtime/pkg/client Client
