0455c3c9c1853dfab3a3e53dcec32ac606807177
BenRub <benrub@gmail.com>
2021-05-02 15:46:40 +0300

    Adding web hook reports tests

--- cbcontainers/monitor/monitor_agent_test.go
+++ cbcontainers/monitor/monitor_agent_test.go
@@ -5,9 +5,13 @@ import (
 	"github.com/golang/mock/gomock"
 	"github.com/vmware/cbcontainers-operator/cbcontainers/monitor/mocks"
 	"github.com/vmware/cbcontainers-operator/cbcontainers/monitor/models"
+	hardeningObjects "github.com/vmware/cbcontainers-operator/cbcontainers/state/hardening/objects"
+	"github.com/vmware/cbcontainers-operator/cbcontainers/test_utils"
 	admissionsV1beta1 "k8s.io/api/admissionregistration/v1beta1"
 	appsV1 "k8s.io/api/apps/v1"
 	coreV1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/types"
 	"testing"
 	"time"
 )
@@ -46,6 +50,56 @@ func testMonitorAgent(t *testing.T, setup SetupFunc) {
 	time.Sleep(SendInterval)
 	agent.Stop()
 	time.Sleep(SendInterval)
+}
+
+func TestWebhookReports(t *testing.T) {
+	testWebhookReports := func(t *testing.T, expectedValidatingWebhooks map[string]admissionsV1beta1.ValidatingWebhookConfiguration, expectedWebhookReports map[string]models.WebhookHealthReport) {
+		testMonitorAgent(t, func(agent *MonitorAgent, testObjects *TestMonitorObjects) models.HealthReportMessage {
+			testObjects.featuresMock.EXPECT().HardeningEnabled().Return(true, nil)
+			testObjects.featuresMock.EXPECT().RuntimeEnabled().Return(true, nil)
+			testObjects.healthCheckerMock.EXPECT().GetPods().Return(map[string]coreV1.Pod{}, nil)
+			testObjects.healthCheckerMock.EXPECT().GetDaemonSets().Return(map[string]appsV1.DaemonSet{}, nil)
+			testObjects.healthCheckerMock.EXPECT().GetDeployments().Return(map[string]appsV1.Deployment{}, nil)
+			testObjects.healthCheckerMock.EXPECT().GetReplicaSets().Return(map[string]appsV1.ReplicaSet{}, nil)
+			testObjects.healthCheckerMock.EXPECT().GetValidatingWebhookConfigurations().Return(expectedValidatingWebhooks, nil)
+
+			return models.NewHealthReportMessage(Account, Cluster, Version, map[string]bool{HardeningFeature: true, RuntimeFeature: true}, map[string]models.WorkloadHealthReport{}, expectedWebhookReports)
+		})
+	}
+
+	t.Run("When there are no validating web hooks, there should be no reports", func(t *testing.T) {
+		testWebhookReports(t, map[string]admissionsV1beta1.ValidatingWebhookConfiguration{}, map[string]models.WebhookHealthReport{})
+	})
+
+	t.Run("When there are validating web hooks but no enforcer web hook, there should be no reports", func(t *testing.T) {
+		testWebhookReports(t, map[string]admissionsV1beta1.ValidatingWebhookConfiguration{
+			test_utils.RandomString(): {
+				Webhooks: []admissionsV1beta1.ValidatingWebhook{{Name: test_utils.RandomString()}},
+			},
+		}, map[string]models.WebhookHealthReport{})
+	})
+
+	t.Run("When there are validating web hooks including the enforcer web hook, there should be one report with the enforcer webhook", func(t *testing.T) {
+		uid := test_utils.RandomString()
+		testWebhookReports(t, map[string]admissionsV1beta1.ValidatingWebhookConfiguration{
+			test_utils.RandomString(): {
+				Webhooks: []admissionsV1beta1.ValidatingWebhook{{Name: test_utils.RandomString()}},
+			},
+			hardeningObjects.EnforcerName: {
+				ObjectMeta: metav1.ObjectMeta{
+					Name: hardeningObjects.EnforcerName,
+					UID:  types.UID(uid),
+				},
+				Webhooks: []admissionsV1beta1.ValidatingWebhook{{Name: test_utils.RandomString()}},
+			},
+		}, map[string]models.WebhookHealthReport{
+			hardeningObjects.EnforcerName: {
+				Type: models.WebhookTypeValidating,
+				Uid:  uid,
+			},
+		})
+	})
+
 }
 
 func TestEnabledComponents(t *testing.T) {
---
+++ cbcontainers/test_utils/random.go
@@ -0,0 +1,24 @@
+package test_utils
+
+import (
+	"math/rand"
+	"time"
+)
+
+func init() {
+	rand.Seed(time.Now().UnixNano())
+}
+
+var letterRunes = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
+
+func RandomString() string {
+	return RandomStringWithLength(6)
+}
+
+func RandomStringWithLength(n int) string {
+	b := make([]rune, n)
+	for i := range b {
+		b[i] = letterRunes[rand.Intn(len(letterRunes))]
+	}
+	return string(b)
+}
