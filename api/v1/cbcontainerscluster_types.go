9839b65a6234be067e40eec5db2021c85b20124d
BenRub <benrub@gmail.com>
2021-08-31 15:11:30 +0300

    Deploy the monitor agent service when apply the cluster CR

--- api/v1/cbcontainerscluster_types.go
+++ api/v1/cbcontainerscluster_types.go
@@ -17,6 +17,7 @@ limitations under the License.
 package v1
 
 import (
+	coreV1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
@@ -27,8 +28,28 @@ import (
 type CBContainersClusterSpec struct {
 	Account           string                        `json:"account,required"`
 	ClusterName       string                        `json:"clusterName,required"`
+	Version           string                        `json:"version,required"`
 	ApiGatewaySpec    CBContainersApiGatewaySpec    `json:"apiGatewaySpec,required"`
 	EventsGatewaySpec CBContainersEventsGatewaySpec `json:"eventsGatewaySpec,required"`
+	// +kubebuilder:default:=<>
+	MonitorSpec CBContainersClusterMonitorSpec `json:"monitorSpec,omitempty"`
+}
+
+type CBContainersClusterMonitorSpec struct {
+	// +kubebuilder:default:=<>
+	Labels map[string]string `json:"labels,omitempty"`
+	// +kubebuilder:default:=<>
+	DeploymentAnnotations map[string]string `json:"deploymentAnnotations,omitempty"`
+	// +kubebuilder:default:=<>
+	PodTemplateAnnotations map[string]string `json:"podTemplateAnnotations,omitempty"`
+	// +kubebuilder:default:={repository:"cbartifactory/monitor"}
+	Image CBContainersImageSpec `json:"image,omitempty"`
+	// +kubebuilder:default:=<>
+	Env map[string]string `json:"env,omitempty"`
+	// +kubebuilder:default:={requests: {memory: "64Mi", cpu: "30m"}, limits: {memory: "256Mi", cpu: "200m"}}
+	Resources coreV1.ResourceRequirements `json:"resources,omitempty"`
+	// +kubebuilder:default:=<>
+	Probes CBContainersHTTPProbesSpec `json:"probes,omitempty"`
 }
 
 // CBContainersClusterStatus defines the observed state of CBContainersCluster
