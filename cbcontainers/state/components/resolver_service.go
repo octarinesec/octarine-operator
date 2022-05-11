ed874cce80ca82d1e3269c696c823ec0cdb91ae8
David Fattakhov <dfattakhov@vmware.com>
2022-05-11 14:59:17 +0300

    delete None

--- cbcontainers/state/components/resolver_service.go
+++ cbcontainers/state/components/resolver_service.go
@@ -36,7 +36,6 @@ func (obj *ResolverServiceK8sObject) MutateK8sObject(k8sObject client.Object, ag
 	resolver := &runtimeProtection.Resolver
 	service.Labels = resolver.Labels
 	service.Spec.Type = coreV1.ServiceTypeClusterIP
-	service.Spec.ClusterIP = coreV1.ClusterIPNone
 
 	service.Spec.Selector = map[string]string{
 		resolverLabelKey: ResolverName,
