f7b5788000ac33575ca3e697e80a63c326475d05
David Fattakhov <dfattakhov@vmware.com>
2023-05-17 14:19:12 +0300

    Added client reader mock

--- cbcontainers/test_utils/mocks/generated.go
+++ cbcontainers/test_utils/mocks/generated.go
@@ -1,3 +1,4 @@
 package mocks
 
 //go:generate mockgen -destination mock_k8s_client.go -package mocks sigs.k8s.io/controller-runtime/pkg/client Client
+//go:generate mockgen -destination mock_k8s_client_reader.go -package mocks sigs.k8s.io/controller-runtime/pkg/client Reader
