68fb9ec46604c524418bdb11e12e436abe126e63
skostov <skostov@vmware.com>
2023-08-24 10:34:41 +0300

    revert

--- docs/clean_cndr.sh
+++ docs/clean_cndr.sh
@@ -5,11 +5,13 @@ if [ $? -eq 0 ]; then
     kubectl delete namespace $NS
 fi
 
+echo "###### Creating $NS namespace ######"
 kubectl create namespace $NS
 
 COUNTER=0
 for node in $(kubectl get nodes --no-headers -o custom-columns=":metadata.name");
 do
+    echo "###### Creating cbcontainers-edr-sensor-cleaner-$COUNTER ######"
     echo "apiVersion: batch/v1
 kind: Job
 metadata:
@@ -35,18 +37,21 @@ spec:
           name: opt-dir
         command: [/bin/sh, -c]
         args:
-        - rm -rf /var/opt/carbonblack /opt/carbonblack
+        - rm -rf /var/opt/carbonblack
       restartPolicy: OnFailure
       nodeName: "$node | kubectl apply -f -
-      COUNTER=$(expr "$COUNTER" + 1)
+      COUNTER=$(expr $COUNTER + 1)
 done
 
 COUNTER=$(expr "$COUNTER" - 1)
 
 for (( c="$COUNTER"; c>=0; c-- ))
 do 
-   kubectl -n $NS wait --for=condition=complete --timeout=60s job/cbcontainers-edr-sensor-cleaner-$c
+   echo "###### Wait for job/cbcontainers-edr-sensor-cleaner-$c to finish ######"
+   kubectl -n $NS wait --for=condition=complete --timeout=60s job/cbcontainers-edr-sensor-cleaner-"$c"
 done
 
+echo "###### Deleting $NS namespace ######"
 kubectl delete namespace $NS
+
 echo "###### DONE ######"
