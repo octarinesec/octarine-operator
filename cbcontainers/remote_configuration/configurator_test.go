e8e09921a2a6bb437fde130c7982370d185f1b3f
Lachezar Tsonov <80523253+ltsonov-cb@users.noreply.github.com>
2023-09-20 15:33:23 +0300

    CNS-2791: Add a remote configurator that pulls changes from CB Console and applies them to the CR (#184)
    * Update README.md
    
    * Update README.md
    
    * Fix charts for main to be idetical to latest release v6.0.1
    
    * Update README.md
    
    * Adding labels field to charts
    
    * Adding company code secret template to charts
    
    * Adding company code secret template to charts fix
    
    * Add documentation for secrets with helm
    
    * config applier skeleton
    
    * More work on the configuration applier loop.
    
    * Happy path test for config_applier
    
    * Pass context to API functions as they'll do IO
    
    * More tests
    
    * Send Failed status to backend when failing to list or update CR
    
    * Treat missing CR when a change is pending as error
    
    * Move "scheduling" logic to a separate struct for config applier
    
    * Use a DummyAPI implementation
    
    * Small refactoring
    
    * Add test for feature toggles
    
    * Add tests for the version field. Split the modification logic in a separate helper function to make testing easier
    
    * Sort the change slice in case there are multiple pending changes
    
    * Clearing TODOs, small fixes
    
    * Removing more TODOs
    
    * Turn off configurator by default and use env var to enable
    
    * Renaming things
    
    * Refactor tests a bit and remove more TODOs
    
    * Change the configurator to read the CR first , so it can extract the cluster name
    
    * Validate change against sensor capabilities
    
    * Validate operator and agent version compatibility
    
    * Tests for positive validation path
    
    * Bring the Validate and Apply change funcs under 1 struct and validate before applying
    
    * Adding change validation to the configuration
    
    * Change validator interface and add fetcher to download metadata from the API
    
    * Add auth_provider and adjust test to match
    
    * Create AccessTokenProvider in main.go
    
    * Extract the ApiGateway from processors.* and use it in configurator as well.
    
    * Go back to the previous version as it looked simpler
    
    * Fixing some tests and main.go
    
    * Change the agent_processor tests to match the new func. Remove a test that was not deterministic
    
    * Add clusterIdentifier parameter to GetConfigurationChanges
    
    * Add clusterID to the status update model call
    
    * Add secret detection to the changer
    
    * Add secret detection to the validation
    
    * Minor TODOs
    
    * Some missing test cases for validation
    
    * More minor changes
    
    * Add sensor metadata implementation. Add dummy config changes implementation.
    
    * Move data classes around
    
    * Removing TODOs
    
    * Change the feature toggle to be part of the CR
    
    * Change errors to have system info and user info for debugging vs user context in the UI
    
    * Small refactor
    
    * Change dummy struct to just a func
    
    * Bump timeout a bit
    
    * Remove controller_test.go
    
    * Add back the env var during development until the feature is completed (so it is not ON by default when testing)
    
    * Add remote_configuration to docker build
    
    * Fix not wrapped error
    
    * Fix missing generate run
    
    * Fix sensors path and response
    
    * Increase iteration sleep time
    
    * Fix helm indentation
    
    * Change the version reset to be more readable
    
    * Move the remote_configuration under cbcontainers and controllers
    
    * Added godoc
    
    ---------
    
    Co-authored-by: BenRub <rubchinskyb@vmware.com>
    Co-authored-by: benrub <benrub@gmail.com>

---
+++ cbcontainers/remote_configuration/configurator_test.go
@@ -0,0 +1,389 @@
+package remote_configuration_test
+
+import (
+	"context"
+	"errors"
+	"github.com/go-logr/logr"
+	"github.com/golang/mock/gomock"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	cbcontainersv1 "github.com/vmware/cbcontainers-operator/api/v1"
+	"github.com/vmware/cbcontainers-operator/cbcontainers/models"
+	"github.com/vmware/cbcontainers-operator/cbcontainers/remote_configuration"
+	"github.com/vmware/cbcontainers-operator/cbcontainers/remote_configuration/mocks"
+	k8sMocks "github.com/vmware/cbcontainers-operator/cbcontainers/test_utils/mocks"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"testing"
+	"time"
+)
+
+type configuratorMocks struct {
+	k8sClient           *k8sMocks.MockClient
+	apiGateway          *mocks.MockApiGateway
+	accessTokenProvider *mocks.MockAccessTokenProvider
+
+	stubAccessToken     string
+	stubOperatorVersion string
+	stubNamespace       string
+	stubClusterID       string
+}
+
+// setupConfigurator sets up mocks and creates a Configurator instance with those mocks and some dummy data
+func setupConfigurator(ctrl *gomock.Controller) (*remote_configuration.Configurator, configuratorMocks) {
+	k8sClient := k8sMocks.NewMockClient(ctrl)
+	apiGateway := mocks.NewMockApiGateway(ctrl)
+	accessTokenProvider := mocks.NewMockAccessTokenProvider(ctrl)
+
+	var mockAPIProvider remote_configuration.ApiCreator = func(
+		cbContainersCluster *cbcontainersv1.CBContainersAgent,
+		accessToken string,
+	) (remote_configuration.ApiGateway, error) {
+		return apiGateway, nil
+	}
+
+	namespace := "namespace-name"
+	accessToken := "access-token"
+	operatorVersion := "1.2.3"
+	clusterID := "1234567"
+	accessTokenProvider.EXPECT().GetCBAccessToken(gomock.Any(), gomock.Any(), namespace).Return(accessToken, nil).AnyTimes()
+
+	configurator := remote_configuration.NewConfigurator(
+		k8sClient,
+		mockAPIProvider,
+		logr.Discard(),
+		accessTokenProvider,
+		operatorVersion,
+		namespace,
+		clusterID,
+	)
+
+	mocksHolder := configuratorMocks{
+		k8sClient:           k8sClient,
+		apiGateway:          apiGateway,
+		accessTokenProvider: accessTokenProvider,
+		stubAccessToken:     accessToken,
+		stubOperatorVersion: operatorVersion,
+		stubNamespace:       namespace,
+		stubClusterID:       clusterID,
+	}
+
+	return configurator, mocksHolder
+}
+
+func TestConfigChangeIsAppliedAndAcknowledgedCorrectly(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	configurator, mocks := setupConfigurator(ctrl)
+
+	// Setup stub data
+	var initialGeneration, finalGeneration int64 = 1, 2
+	expectedAgentVersion := "3.0.0"
+	cr := &cbcontainersv1.CBContainersAgent{ObjectMeta: metav1.ObjectMeta{Generation: initialGeneration}}
+	configChange := randomPendingConfigChange()
+	configChange.AgentVersion = &expectedAgentVersion
+
+	setupCRInK8S(mocks.k8sClient, cr)
+	setupValidCompatibilityData(mocks.apiGateway, expectedAgentVersion, mocks.stubOperatorVersion)
+	mocks.apiGateway.EXPECT().GetConfigurationChanges(gomock.Any(), mocks.stubClusterID).Return([]models.ConfigurationChange{configChange}, nil)
+
+	// Setup mock assertions
+	mocks.apiGateway.EXPECT().UpdateConfigurationChangeStatus(gomock.Any(), gomock.Any()).DoAndReturn(func(_ context.Context, update models.ConfigurationChangeStatusUpdate) error {
+		assert.Equal(t, configChange.ID, update.ID)
+		assert.Equal(t, finalGeneration, update.AppliedGeneration)
+		assert.Equal(t, models.ChangeStatusAcked, update.Status)
+		assert.NotEmpty(t, update.AppliedTimestamp, "applied timestamp should be populated")
+		assert.Equal(t, mocks.stubClusterID, update.ClusterIdentifier)
+
+		parsedTime, err := time.Parse(time.RFC3339, update.AppliedTimestamp)
+		assert.NoError(t, err)
+		assert.True(t, time.Now().After(parsedTime))
+		return nil
+	})
+
+	setupUpdateCRMock(t, mocks.k8sClient, func(agent *cbcontainersv1.CBContainersAgent) {
+		assert.Equal(t, expectedAgentVersion, agent.Spec.Version)
+		agent.ObjectMeta.Generation = finalGeneration
+	})
+
+	err := configurator.RunIteration(context.Background())
+	assert.NoError(t, err)
+}
+
+func TestWhenChangeIsNotApplicableShouldReturnError(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	configurator, mocks := setupConfigurator(ctrl)
+
+	cr := &cbcontainersv1.CBContainersAgent{}
+	maxAgentVersionForOperator := "4.0.0"
+	agentVersion := "5.0.0"
+	configChange := randomPendingConfigChange()
+	configChange.AgentVersion = &agentVersion
+
+	setupCRInK8S(mocks.k8sClient, cr)
+	mocks.apiGateway.EXPECT().GetConfigurationChanges(gomock.Any(), mocks.stubClusterID).Return([]models.ConfigurationChange{configChange}, nil)
+
+	// Setup invalid compatibility; no need to do full verification here - this is what the validator tests are for
+	// We just want to check that _some_ validation happens
+	mocks.apiGateway.EXPECT().GetSensorMetadata().Return([]models.SensorMetadata{{Version: agentVersion}}, nil)
+	mocks.apiGateway.EXPECT().GetCompatibilityMatrixEntryFor(mocks.stubOperatorVersion).Return(&models.OperatorCompatibility{
+		MinAgent: models.AgentMinVersionNone,
+		MaxAgent: models.AgentVersion(maxAgentVersionForOperator),
+	}, nil)
+
+	// Setup mock assertions
+	mocks.apiGateway.EXPECT().UpdateConfigurationChangeStatus(gomock.Any(), gomock.Any()).
+		DoAndReturn(func(_ context.Context, update models.ConfigurationChangeStatusUpdate) error {
+			assert.Equal(t, configChange.ID, update.ID)
+			assert.Equal(t, models.ChangeStatusFailed, update.Status)
+			assert.NotEmpty(t, update.Error)
+			assert.NotEmpty(t, update.ErrorReason)
+			assert.Equal(t, int64(0), update.AppliedGeneration)
+			assert.Empty(t, update.AppliedTimestamp)
+			assert.Equal(t, mocks.stubClusterID, update.ClusterIdentifier)
+
+			return nil
+		})
+
+	err := configurator.RunIteration(context.Background())
+	assert.Error(t, err)
+}
+
+func TestWhenThereAreNoPendingChangesNothingHappens(t *testing.T) {
+	testCases := []struct {
+		name            string
+		dataFromService []models.ConfigurationChange
+	}{
+		{
+			name:            "empty list",
+			dataFromService: []models.ConfigurationChange{},
+		},
+		{
+			name: "list is not empty but there are no PENDING changes",
+			dataFromService: []models.ConfigurationChange{
+				{ID: "123", Status: "non-existent"},
+				{ID: "234", Status: "FAILED"},
+				{ID: "345", Status: "ACKNOWLEDGED"},
+				{ID: "456", Status: "SUCCEEDED"},
+			},
+		},
+	}
+
+	for _, tC := range testCases {
+		t.Run(tC.name, func(t *testing.T) {
+			ctrl := gomock.NewController(t)
+			defer ctrl.Finish()
+
+			configurator, mocks := setupConfigurator(ctrl)
+
+			setupCRInK8S(mocks.k8sClient, nil)
+			mocks.apiGateway.EXPECT().GetConfigurationChanges(gomock.Any(), mocks.stubClusterID).Return(tC.dataFromService, nil)
+			mocks.apiGateway.EXPECT().UpdateConfigurationChangeStatus(gomock.Any(), gomock.Any()).Times(0)
+
+			err := configurator.RunIteration(context.Background())
+			assert.NoError(t, err)
+		})
+	}
+}
+
+func TestWhenThereAreMultiplePendingChangesTheOldestIsSelected(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	configurator, mocks := setupConfigurator(ctrl)
+
+	olderChange := randomPendingConfigChange()
+	newerChange := randomPendingConfigChange()
+
+	expectedVersion := "version-for-older-change"
+	versionThatShouldNotBe := "version-for-newer-change"
+	olderChange.AgentVersion = &expectedVersion
+	newerChange.AgentVersion = &versionThatShouldNotBe
+	olderChange.Timestamp = time.Now().UTC().Add(-2 * time.Hour).Format(time.RFC3339)
+	newerChange.Timestamp = time.Now().UTC().Add(-1 * time.Hour).Format(time.RFC3339)
+
+	setupCRInK8S(mocks.k8sClient, nil)
+	mocks.apiGateway.EXPECT().GetConfigurationChanges(gomock.Any(), mocks.stubClusterID).Return([]models.ConfigurationChange{newerChange, olderChange}, nil)
+	setupValidCompatibilityData(mocks.apiGateway, expectedVersion, mocks.stubOperatorVersion)
+
+	setupUpdateCRMock(t, mocks.k8sClient, func(agent *cbcontainersv1.CBContainersAgent) {
+		assert.Equal(t, expectedVersion, agent.Spec.Version)
+	})
+
+	mocks.apiGateway.EXPECT().UpdateConfigurationChangeStatus(gomock.Any(), gomock.Any()).
+		DoAndReturn(func(_ context.Context, update models.ConfigurationChangeStatusUpdate) error {
+			assert.Equal(t, olderChange.ID, update.ID)
+			return nil
+		})
+
+	err := configurator.RunIteration(context.Background())
+	assert.NoError(t, err)
+}
+
+func TestWhenConfigurationAPIReturnsErrorForListShouldPropagateErr(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	configurator, mocks := setupConfigurator(ctrl)
+
+	setupCRInK8S(mocks.k8sClient, nil)
+
+	errFromService := errors.New("some error")
+	mocks.apiGateway.EXPECT().GetConfigurationChanges(gomock.Any(), mocks.stubClusterID).Return(nil, errFromService)
+
+	returnedErr := configurator.RunIteration(context.Background())
+
+	assert.Error(t, returnedErr)
+	assert.ErrorIs(t, returnedErr, errFromService, "expected returned error to match or wrap error from service")
+}
+
+func TestWhenGettingCRFromAPIServerFailsAnErrorIsReturned(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	configurator, mocks := setupConfigurator(ctrl)
+
+	errFromService := errors.New("some error")
+	mocks.k8sClient.EXPECT().List(gomock.Any(), &cbcontainersv1.CBContainersAgentList{}).Return(errFromService)
+
+	returnedErr := configurator.RunIteration(context.Background())
+	assert.Error(t, returnedErr)
+	assert.ErrorIs(t, returnedErr, errFromService, "expected returned error to match or wrap error from service")
+}
+
+func TestWhenUpdatingCRFailsChangeIsUpdatedAsFailed(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	configurator, mocks := setupConfigurator(ctrl)
+
+	configChange := randomPendingConfigChange()
+
+	mocks.apiGateway.EXPECT().GetConfigurationChanges(gomock.Any(), mocks.stubClusterID).Return([]models.ConfigurationChange{configChange}, nil)
+	setupCRInK8S(mocks.k8sClient, nil)
+	setupValidCompatibilityData(mocks.apiGateway, *configChange.AgentVersion, mocks.stubOperatorVersion)
+
+	errFromService := errors.New("some error")
+	mocks.k8sClient.EXPECT().Update(gomock.Any(), gomock.Any()).Return(errFromService)
+
+	mocks.apiGateway.EXPECT().UpdateConfigurationChangeStatus(gomock.Any(), gomock.Any()).
+		DoAndReturn(func(_ context.Context, update models.ConfigurationChangeStatusUpdate) error {
+			assert.Equal(t, configChange.ID, update.ID)
+			assert.Equal(t, models.ChangeStatusFailed, update.Status)
+			assert.NotEmpty(t, update.Error)
+			assert.Empty(t, update.ErrorReason)
+			assert.Equal(t, int64(0), update.AppliedGeneration)
+			assert.Empty(t, update.AppliedTimestamp)
+			assert.Equal(t, mocks.stubClusterID, update.ClusterIdentifier)
+
+			return nil
+		})
+
+	returnedErr := configurator.RunIteration(context.Background())
+	assert.Error(t, returnedErr)
+	assert.ErrorIs(t, returnedErr, errFromService, "expected returned error to match or wrap error from service")
+}
+
+func TestWhenUpdatingStatusToBackendFailsShouldReturnError(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	configurator, mocks := setupConfigurator(ctrl)
+
+	configChange := randomPendingConfigChange()
+
+	setupCRInK8S(mocks.k8sClient, nil)
+	setupValidCompatibilityData(mocks.apiGateway, *configChange.AgentVersion, mocks.stubOperatorVersion)
+	mocks.apiGateway.EXPECT().GetConfigurationChanges(gomock.Any(), mocks.stubClusterID).Return([]models.ConfigurationChange{configChange}, nil)
+	mocks.k8sClient.EXPECT().Update(gomock.Any(), gomock.Any(), gomock.Any()).Return(nil)
+
+	errFromService := errors.New("some error")
+	mocks.apiGateway.EXPECT().UpdateConfigurationChangeStatus(gomock.Any(), gomock.Any()).Return(errFromService)
+
+	returnedErr := configurator.RunIteration(context.Background())
+	assert.Error(t, returnedErr)
+	assert.ErrorIs(t, errFromService, returnedErr, "expected returned error to match or wrap error from service")
+}
+
+func TestWhenThereIsNoCRInstalledNothingHappens(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	configurator, mocks := setupConfigurator(ctrl)
+
+	mocks.k8sClient.EXPECT().List(gomock.Any(), &cbcontainersv1.CBContainersAgentList{}).
+		Do(func(ctx context.Context, list *cbcontainersv1.CBContainersAgentList, _ ...any) {
+			list.Items = []cbcontainersv1.CBContainersAgent{}
+		})
+
+	// No other mock calls should happen without a CR
+	assert.NoError(t, configurator.RunIteration(context.Background()))
+}
+
+func TestWhenFeatureIsDisabledInCRNothingHappens(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	configurator, mocks := setupConfigurator(ctrl)
+
+	cr := &cbcontainersv1.CBContainersAgent{
+		Spec: cbcontainersv1.CBContainersAgentSpec{
+			Components: cbcontainersv1.CBContainersComponentsSpec{
+				Settings: cbcontainersv1.CBContainersComponentsSettings{
+					RemoteConfiguration: &cbcontainersv1.CBContainersRemoteConfigurationSettings{
+						EnabledForAgent: falsePtr,
+					},
+				}},
+		},
+	}
+	setupCRInK8S(mocks.k8sClient, cr)
+
+	// No other mock calls should happen once we know the feature is disabled
+	assert.NoError(t, configurator.RunIteration(context.Background()))
+}
+
+// setupCRInK8S ensures the mock client will return 1 agent item for List calls - either the provided one or an empty CR otherwise
+func setupCRInK8S(mock *k8sMocks.MockClient, item *cbcontainersv1.CBContainersAgent) *cbcontainersv1.CBContainersAgent {
+	if item == nil {
+		item = &cbcontainersv1.CBContainersAgent{
+			TypeMeta:   metav1.TypeMeta{},
+			ObjectMeta: metav1.ObjectMeta{},
+			Spec:       cbcontainersv1.CBContainersAgentSpec{},
+			Status:     cbcontainersv1.CBContainersAgentStatus{},
+		}
+	}
+	mock.EXPECT().List(gomock.Any(), &cbcontainersv1.CBContainersAgentList{}).
+		Do(func(ctx context.Context, list *cbcontainersv1.CBContainersAgentList, _ ...any) {
+			list.Items = []cbcontainersv1.CBContainersAgent{*item}
+		})
+
+	return item
+}
+
+func setupUpdateCRMock(t *testing.T, mock *k8sMocks.MockClient, assert func(*cbcontainersv1.CBContainersAgent)) {
+	mock.EXPECT().Update(gomock.Any(), gomock.Any(), gomock.Any()).
+		DoAndReturn(func(_ context.Context, item any, _ ...any) error {
+			asCb, ok := item.(*cbcontainersv1.CBContainersAgent)
+			require.True(t, ok)
+
+			assert(asCb)
+			return nil
+		})
+}
+
+func setupValidCompatibilityData(mockGateway *mocks.MockApiGateway, sensorVersion, operatorVersion string) {
+	mockGateway.EXPECT().GetSensorMetadata().Return([]models.SensorMetadata{{
+		Version:                 sensorVersion,
+		SupportsRuntime:         true,
+		SupportsClusterScanning: true,
+		SupportsCndr:            true,
+	}}, nil)
+
+	mockGateway.EXPECT().GetCompatibilityMatrixEntryFor(operatorVersion).Return(&models.OperatorCompatibility{
+		MinAgent: models.AgentMinVersionNone,
+		MaxAgent: models.AgentMaxVersionLatest,
+	}, nil)
+
+}
--- controllers/mocks/generated.go
+++ controllers/mocks/generated.go
@@ -1,4 +1,5 @@
 package mocks
 
 //go:generate mockgen -destination mock_state_applier.go -package mocks github.com/vmware/cbcontainers-operator/controllers StateApplier
-//go:generate mockgen -destination mock_cluster_processor.go -package mocks github.com/vmware/cbcontainers-operator/controllers ClusterProcessor
+//go:generate mockgen -destination mock_agent_processor.go -package mocks github.com/vmware/cbcontainers-operator/controllers AgentProcessor
+//go:generate mockgen -destination mock_access_token_provider.go -package mocks github.com/vmware/cbcontainers-operator/controllers AccessTokenProvider
