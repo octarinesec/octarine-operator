e32b75381b3748f37279c3d21ea68e170e35650b
benrub <benrub@gmail.com>
2023-10-19 12:17:06 +0300

    Revert "add /lib/modules and /usr/src as volume mounts."
    This reverts commit 03862c787068b10c287374c41817c2e24bac2483.

--- cbcontainers/state/components/sensor_daemon_set.go
+++ cbcontainers/state/components/sensor_daemon_set.go
@@ -77,8 +77,6 @@ var (
 	hostPathFile              = coreV1.HostPathFile
 	cndrHostPaths             = map[string]*coreV1.HostPathVolumeSource{
 		"boot":        {Path: "/boot", Type: &hostPathDirectory},
-		"modules":     {Path: "/lib/modules", Type: &hostPathDirectory},
-		"src":         {Path: "/usr/src", Type: &hostPathDirectory},
 		"cb-data-dir": {Path: "/var/opt/carbonblack", Type: &hostPathDirectoryOrCreate},
 		"os-release":  {Path: "/etc/os-release", Type: &hostPathFile},
 		"root":        {Path: "/", Type: &hostPathDirectory},
@@ -90,8 +88,6 @@ var (
 	cndrReadOnlyMounts = map[string]struct{}{
 		"root":       {},
 		"boot":       {},
-		"modules":    {},
-		"src":        {},
 		"os-release": {},
 	}
 )
@@ -125,7 +121,7 @@ func (obj *SensorDaemonSetK8sObject) MutateK8sObject(k8sObject client.Object, ag
 
 	obj.initiateDaemonSet(daemonSet, agentSpec)
 
-	if commonState.IsEnabled(runtimeProtection.Enabled) || isCndrEnabled(agentSpec.Components.Cndr) {
+	if commonState.IsEnabled(runtimeProtection.Enabled) || isCndrEnbaled(agentSpec.Components.Cndr) {
 		daemonSet.Spec.Template.Spec.DNSPolicy = runtimeSensorDNSPolicy
 		daemonSet.Spec.Template.Spec.HostNetwork = runtimeSensorHostNetwork
 		daemonSet.Spec.Template.Spec.HostPID = runtimeSensorHostPID
@@ -213,14 +209,14 @@ func (obj *SensorDaemonSetK8sObject) mutateAnnotations(daemonSet *appsV1.DaemonS
 	}
 }
 
-func isCndrEnabled(cndrSpec *cbContainersV1.CBContainersCndrSpec) bool {
+func isCndrEnbaled(cndrSpec *cbContainersV1.CBContainersCndrSpec) bool {
 	return cndrSpec != nil && commonState.IsEnabled(cndrSpec.Enabled)
 }
 
 func (obj *SensorDaemonSetK8sObject) getExpectedVolumeCount(agentSpec *cbContainersV1.CBContainersAgentSpec) int {
 	expectedVolumesCount := 0
 
-	if commonState.IsEnabled(agentSpec.Components.ClusterScanning.Enabled) || isCndrEnabled(agentSpec.Components.Cndr) {
+	if commonState.IsEnabled(agentSpec.Components.ClusterScanning.Enabled) || isCndrEnbaled(agentSpec.Components.Cndr) {
 		expectedVolumesCount += len(supportedContainerRuntimes)
 	}
 
@@ -235,7 +231,7 @@ func (obj *SensorDaemonSetK8sObject) getExpectedVolumeCount(agentSpec *cbContain
 		expectedVolumesCount += 3
 	}
 
-	if isCndrEnabled(agentSpec.Components.Cndr) {
+	if isCndrEnbaled(agentSpec.Components.Cndr) {
 		expectedVolumesCount += len(cndrHostPaths)
 	}
 
@@ -253,7 +249,7 @@ func (obj *SensorDaemonSetK8sObject) mutateVolumes(daemonSet *appsV1.DaemonSet, 
 		templatePodSpec.Volumes = make([]coreV1.Volume, 0, expectedVolumeCount)
 	}
 
-	if commonState.IsEnabled(agentSpec.Components.ClusterScanning.Enabled) || isCndrEnabled(agentSpec.Components.Cndr) {
+	if commonState.IsEnabled(agentSpec.Components.ClusterScanning.Enabled) || isCndrEnbaled(agentSpec.Components.Cndr) {
 		obj.mutateContainerRuntimesVolumes(&daemonSet.Spec.Template.Spec)
 	}
 
@@ -261,7 +257,7 @@ func (obj *SensorDaemonSetK8sObject) mutateVolumes(daemonSet *appsV1.DaemonSet, 
 		obj.mutateClusterScannerVolumes(&daemonSet.Spec.Template.Spec, &agentSpec.Components.ClusterScanning.ClusterScannerAgent)
 	}
 
-	if isCndrEnabled(agentSpec.Components.Cndr) {
+	if isCndrEnbaled(agentSpec.Components.Cndr) {
 		obj.mutateCndrVolumes(&daemonSet.Spec.Template.Spec, &agentSpec.Components.Cndr.Sensor)
 	}
 
@@ -313,7 +309,7 @@ func (obj *SensorDaemonSetK8sObject) mutateContainersList(daemonSet *appsV1.Daem
 		desiredContainers = append(desiredContainers, clusterScannerContainer)
 	}
 
-	if isCndrEnabled(agentSpec.Components.Cndr) {
+	if isCndrEnbaled(agentSpec.Components.Cndr) {
 		cndrEnabled = true
 		if cndrContainerLocation := obj.findContainerLocationByName(templatePodSpec.Containers, CndrContainerName); cndrContainerLocation == -1 {
 			cndrMissing = true
@@ -341,7 +337,7 @@ func (obj *SensorDaemonSetK8sObject) mutateContainersList(daemonSet *appsV1.Daem
 			agentSpec)
 	}
 
-	if isCndrEnabled(agentSpec.Components.Cndr) {
+	if isCndrEnbaled(agentSpec.Components.Cndr) {
 		obj.mutateCndrContainer(
 			&templatePodSpec.Containers[obj.findContainerLocationByName(templatePodSpec.Containers, CndrContainerName)],
 			agentSpec)
